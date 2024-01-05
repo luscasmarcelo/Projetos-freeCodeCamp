58b9cc4448cc4c0a63b3ad9d5d5d5837f040e922
Lucas Marcelo <lucas.sampaio4@gmail.com>
2024-01-05 10:04:14 -0300

    validador simples em js

---
+++ validador de telefone/index.js
@@ -0,0 +1,10 @@
+//um validador simples de numero de telefone dos eua, onde o sistema verifica se a string fornecida está
+//de acordo com o esquema de numeros padrão: xxx-xxx-xxxx
+
+function telephoneCheck(str) {
+    let requisitoNumero = /^(\+1)?[ -]?\(?([0-9]{3})\)?[ -]?([0-9]{3})[ -]?([0-9]{4})$/;
+  
+    return requisitoNumero.test(str)
+  }
+  
+  console.log(telephoneCheck("555-555-5555"));
